bbd2ca5768c332415d6d0b8249a1b60cdb5e4248
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2019-03-04 18:04:39 -0600

    Attempted to figure out on my own, had to look up solution

--- IntExtension.playground/Contents.swift
+++ IntExtension.playground/Contents.swift
@@ -1,3 +1,20 @@
-import Cocoa
+import Foundation
 
-var str = "Hello, playground"
+extension Int {
+    func add(_ num: String) -> Int? {
+        
+        guard let myInt = Int(num) else {
+            return nil
+        }
+        return self + myInt
+    }
+}
+
+let value1: Int? = 3.add("4")
+let value2: Int? = 2.add("Hello World")
+
+
+
+
+
+
